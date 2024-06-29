819161e55d2c258e877c830dfe2c5a659784ee79
prabha-lumel <103419137+prabha-lumel@users.noreply.github.com>
2024-06-29 19:24:48 +0530

    Update Home.js

--- code-tool/src/pages/Home.js
+++ code-tool/src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="container">
-      <h1>ValQ3.0 Coding Standard Tool</h1>
+      <h1>Automation Coding Standard Tool</h1>
       <div>
         <Link to="/guidance" className="cta-button">Add JS Doc and I.say</Link>
       </div>
